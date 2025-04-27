b6354cba3916051ca1faa2a4192862a30c290dd0
frankiyyaa <firanolbabu@gmail.com>
2025-04-27 08:57:39 -0700

    updated and created listing form

--- House_Renting_Platform/Listing/models.py
+++ House_Renting_Platform/Listing/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
-# Create your models here.
+class listing(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=True, blank=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=255)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    parking = models.BooleanField(default=False)
+    property_type = models.CharField(max_length=50, choices=[('Apartment', 'Apartment'), ('House', 'House'), ('Condo', 'Condo')], blank=True)
+    property_size = models.DecimalField(max_digits=10, decimal_places=2,blank=True)
+    furnished = models.BooleanField(default=False)
+    available = models.BooleanField(default=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
+    def __str__(self): 
+        return self.title
+    
+    
--- House_Renting_Platform/Listing/views.py
+++ House_Renting_Platform/Listing/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .forms import ListingForm
 
 # Create your views here.
 def index(request):
-    
-    return render(request, 'index.html', {})
+    form= ListingForm 
+    return render(request, 'listing.html', {
+        'form': form,
+    })
