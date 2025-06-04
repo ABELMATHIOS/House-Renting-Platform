a4184a0f2f94a854455c5af67fb04aa6f754ff77
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-06-04 03:07:23 -0700

    minor edit

--- House_Renting_Platform/manage.py
+++ House_Renting_Platform/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'house_renting_platform.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'House_Renting_Rlatform.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
