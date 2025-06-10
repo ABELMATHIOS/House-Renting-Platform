30a7155839eda8c739977e3b11b4bbb254a171fc
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-06-10 04:53:32 -0700

    detailed property view done

--- House_Renting_Platform/Listing/views.py
+++ House_Renting_Platform/Listing/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import ListingForm
 from .models import ListingModel
+from django.contrib import messages
 
 
 def landing(request):
@@ -8,13 +9,14 @@ def landing(request):
     })
 
 
-def index(request):
+def new_property(request):
     if request.method == 'POST':
      form = ListingForm(request.POST, request.FILES)
      if form.is_valid():
         new_property = form.save(commit=False)
         new_property.save()
-        return redirect('Listing:landing')
+        messages.success(request, "Your property is listed succesfully!!!")
+        return redirect('index')
     else:
       form= ListingForm()
     return render(request, 'add-property.html', {
@@ -27,6 +29,12 @@ def listed_properties(request):
     listed_properties = ListingModel.objects.all()
     return render(request, 'property-halfmap-list.html', {
        'listed_properties': listed_properties,
+    })
+
+def view_property_details(request, id):
+    current_property = ListingModel.objects.get(id=id)
+    return render(request, 'property-details-v4.html', {
+       'current_property': current_property,
     })
 
 
