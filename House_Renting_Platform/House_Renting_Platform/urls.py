3cae44d9a3a90db7c46c90375d5cf36644606e95
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2025-07-29 22:08:48 +0300

    feature: integrated listing properties with payment so that properties will not get listed unless payment is succesfull
    Co-authored-by: Abel Mathios <7abelgebru@gmail.com>

--- House_Renting_Platform/House_Renting_Platform/urls.py
+++ House_Renting_Platform/House_Renting_Platform/urls.py
@@ -15,4 +15,4 @@ urlpatterns = [
     path('profile/', include('accounts.urls')),
 ]
 
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- House_Renting_Platform/Listing/urls.py
+++ House_Renting_Platform/Listing/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('new-property', views.new_property, name='new-property' ),
     path('listed-properties', views.listed_properties, name='listed-properties'),
     path('view-property-details/<int:id>', views.view_property_details, name='view-property-details'),
-    path('landing', views.landing, name='landing')
 ]
